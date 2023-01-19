fd9ccadf41709dcc5cc51d0fcd1d4495d2184526
0xspringtime <110655352+0xspringtime@users.noreply.github.com>
2023-01-19 03:29:09 -0500

    Delete hello_cargo-86b856d3e5cfd6ec.d

--- hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec.d
+++
@@ -1,5 +0,0 @@
-/home/pat/rust/hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec: src/main.rs
-
-/home/pat/rust/hello_cargo/target/debug/deps/hello_cargo-86b856d3e5cfd6ec.d: src/main.rs
-
-src/main.rs:
