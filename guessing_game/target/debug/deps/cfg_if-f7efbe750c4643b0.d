06465df0e0b6eefa52ce1675287acd2b8f1cea25
0xspringtime <110655352+0xspringtime@users.noreply.github.com>
2023-01-19 03:28:10 -0500

    Delete guessing_game/target/debug/deps directory

--- guessing_game/target/debug/deps/cfg_if-f7efbe750c4643b0.d
+++
@@ -1,7 +0,0 @@
-/home/pat/rust/guessing_game/target/debug/deps/cfg_if-f7efbe750c4643b0.rmeta: /home/pat/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/pat/rust/guessing_game/target/debug/deps/libcfg_if-f7efbe750c4643b0.rlib: /home/pat/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/pat/rust/guessing_game/target/debug/deps/cfg_if-f7efbe750c4643b0.d: /home/pat/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/pat/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
