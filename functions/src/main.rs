2128235bc77e513b037ad8a5096acff0c8c5b2c2
0xspringtime <0xspringtime@protonmail.com>
2023-01-27 06:06:24 -0500

    commit

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    print_labeled_measurement(5, 'h');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
 
